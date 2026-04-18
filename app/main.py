ba682803ad95b807200430cb446d29da095bad5b
Lxnthz <byanfidry@gmail.com>
2026-04-19 00:09:38 +0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -5,11 +5,9 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    # Uncomment the code below to pass the first stage
-    
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
-
+    server_socket = socket.create_server(("localhost", 6379), reuse_port=True) # [STAGE 1] Create a server socket and wait for a client to connect.
+    connection, _ = server_socket.accept() # [STAGE 2] Accept a client connection, _ is the address of the client, which we can ignore for this task.
+    connection.sendall(b"+PONG\r\n") # [STAGE 2] Send the "+PONG\r\n" response to the client. `b` prefix convert the string to a bytes object, which is required for sending data over a socket.
 
 if __name__ == "__main__":
     main()
