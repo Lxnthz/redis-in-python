42212a6a3a565e2afa477f3d4682c15fd3b6e88a
Lxnthz <byanfidry@gmail.com>
2026-04-21 17:25:19 +0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1875,7 +1875,7 @@ def execute_command(connection, selector, command_parts, raw_command=None, send_
 
     if entry is None:
       if send_response:
-        connection.sendall(encode_array(None))
+        connection.sendall(encode_array([None for _ in command_parts[2:]]))
       return True
 
     response = []
